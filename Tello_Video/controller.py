7ebd0b8163cae82ca5ef0c0986aa0b86d4123571
zflint <32756823+jzfcoder@users.noreply.github.com>
2023-03-24 18:19:08 +0000

    mono_odometry impl

--- Tello_Video/controller.py
+++ Tello_Video/controller.py
@@ -1,5 +1,8 @@
 import tello
 import PIDController
+import mono_video_odometry
+import numpy as np
+import cv2 as cv
 
 class Controller:
     def __init__(self):
@@ -14,6 +17,18 @@ class Controller:
         self.xpid = PIDController.PIDController(8, 0.02, 10, 75, 75, 100, 100)
         self.ypid = PIDController.PIDController(4, 2.35, 8, 75, 75, 100, 100)
         self.zpid = PIDController.PIDController(8, 0.02, 10, 75, 75, 100, 100)
+
+        focal = 718.8560
+        pp = (607.1928, 185.2157)
+        R_total = np.zeros((3, 3))
+        t_total = np.empty(shape=(3, 1))
+
+        # Parameters for lucas kanade optical flow
+        lk_params = dict( winSize  = (21,21),
+                  criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 30, 0.01))
+
+
+        self.vo = mono_video_odometry.MonoVideoOdometery('img', 'pose', focal, pp, lk_params)
 
     def send_command(self, cmd):
         if cmd.contains("takeoff") or cmd.contains("land"):
@@ -35,7 +50,11 @@ class Controller:
         else:
             self.current_pose = self.process_frame()
             correction = self.calculate_correction(self.current_pose[0], self.current_pose[1], self.current_pose[2])
-            self.drone.send_command('go {} {} {} 10'.format(self.target_pose[0] + correction[0], self.target_pose[1] + correction[1], self.target_pose[2] + correction[2]))
+            self.target_pose[0] = correction[0]
+            self.target_pose[1] = correction[1]
+            self.target_pose[2] = correction[2]
+
+            self.drone.send_command('go {} {} {} 10'.format(self.target_pose[0], self.target_pose[1], self.target_pose[2]))
 
     def process_frame(self):
         return [0, 0, 0]
